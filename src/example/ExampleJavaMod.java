81b9597dbe943ca995436801aabff49bb94b9581
Tarlan2347 <120419593+Tarlan2347@users.noreply.github.com>
2022-12-17 13:30:23 -0900

    Update ExampleJavaMod.java

--- src/example/ExampleJavaMod.java
+++ src/example/ExampleJavaMod.java
@@ -9,28 +9,19 @@ import mindustry.gen.*;
 import mindustry.mod.*;
 import mindustry.ui.dialogs.*;
 
-public class ExampleJavaMod extends Mod{
+public class Extendustry extends Mod{
 
-    public ExampleJavaMod(){
-        Log.info("Loaded ExampleJavaMod constructor.");
+    public Extendustry(){
+        Log.info("Loaded Extendustry constructor.");
 
         //listen for game load event
         Events.on(ClientLoadEvent.class, e -> {
-            //show dialog upon startup
-            Time.runTask(10f, () -> {
-                BaseDialog dialog = new BaseDialog("frog");
-                dialog.cont.add("behold").row();
-                //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
-                dialog.cont.image(Core.atlas.find("example-java-mod-frog")).pad(20f).row();
-                dialog.cont.button("I see", dialog::hide).size(100f, 50f);
-                dialog.show();
-            });
         });
     }
 
     @Override
     public void loadContent(){
-        Log.info("Loading some example content.");
+        Log.info("Loading Extendustry Content.");
     }
 
 }
