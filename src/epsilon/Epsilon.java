a452ca24eb7f8a69378f5f36aa727ff27ca50872
Tarlan <shadowsans3789@gmail.com>
2022-12-18 16:00:08 -0900

    Simplified for testing

--- src/epsilon/Epsilon.java
+++ src/epsilon/Epsilon.java
@@ -1,15 +1,7 @@
 package epsilon;
 
-import arc.Core;
-import arc.Events;
-import arc.util.Log;
-import arc.util.Time;
 import epsilon.content.EpsilonItems;
-import epsilon.content.EpsilonPlanets;
-import epsilon.content.EpsilonTechTree;
-import mindustry.game.EventType;
 import mindustry.mod.*;
-import mindustry.ui.dialogs.BaseDialog;
 
 public class Epsilon extends Mod{
 
@@ -18,7 +10,5 @@ public class Epsilon extends Mod{
     @Override
     public void loadContent() {
         EpsilonItems.load();
-        //EpsilonPlanets.load();
-        //EpsilonTechTree.load();
     }
 }
--- src/epsilon/content/EpsilonItems.java
+++ src/epsilon/content/EpsilonItems.java
@@ -1,22 +1,15 @@
 package epsilon.content;
 
 import arc.graphics.Color;
-import arc.struct.Seq;
-import mindustry.content.Items;
 import mindustry.type.*;
-
-import static mindustry.content.Items.*;
 
 public class EpsilonItems{
     public static Item
             boron;
-    //public static final Seq<Item> epsilonItems = new Seq<>(), epsilonOnlyItems = new Seq<>();
 
     public static void load(){
-        boron = new Item("boron", Color.valueOf("858585")){};
+        boron = new Item("boron", Color.valueOf("858585")){
 
-        // epsilonItems.addAll(copper, lead, metaglass, coal, titanium, thorium, silicon, surgeAlloy, sand, tungsten, oxide, carbide, boron);
-        //epsilonOnlyItems.addAll(epsilonItems).removeAll(serpuloItems);
-        //epsilonOnlyItems.addAll(epsilonItems).removeAll(erekirItems);
+        };
     }
 }
